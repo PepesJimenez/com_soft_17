b7df9d203d4cf601ef10479a82803b83e28817f1
PepesJimenez <jimenezmoralesjoseantonio97@gmail.com>
2017-03-04 19:09:32 +0100

    Codigos s3
    Añadidos codigos de la sesion 3

---
+++ s3/aritmetica.c
@@ -0,0 +1,37 @@
+
+
+
+#inlude <stdio.h>
+
+
+int main () {
+
+    int numbers[5];
+    int *p;
+    
+    p = numbers;        //apunta a la primera direccion del array
+    *p = 10;            //inicializa la pos. 0 del array a 10
+    
+    p++;                // avanzo a la siguient eposicion del array
+    *p = 20;            // inicializa la pos. 2 a 20
+    
+    p = &numbers[2];    //cambio p por la posicion 2 del array
+    *p = 30;            // inicializa la pos. 2 del array a 30
+    
+    p = numbers + 3;    //apunto a la posicion 3
+    *p = 40;            // inicializa la pos. 3 del array a 40
+ 
+    p = numbers;
+    *(p + 4) = 54;      // inicializa la pos. 4 del arrray a 54
+    
+    
+    printf ("Array inicializado = [");
+    for (int i = 0; i < 1; i++) {
+        
+        printf("%d", numbers[i]);
+    }
+    printf("%d]\n", numbers[4]); 
+ 
+    
+    return 0;
+}
---
+++ s3/variacionesReservaMemoria.c
@@ -0,0 +1,60 @@
+
+/*
+* Programa que solicita un nº de enteros y reserva
+* memoria dinamica para ellos con un array
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLINE 256
+
+
+
+int main() {
+    
+    //variables
+    int MAX_TAM = 0;
+    char input[MAXLINE];
+    
+    //solicitamos el tamaño del array
+    printf("Introduce el tamaño del array: ");
+    fgets(input, MAXLINE, stdin);
+    sscanf(input, "%d%", &MAX_TAM);
+    
+    //reservamos memoria
+    int *vector = (int *) malloc(MAX_TAM * sizeof(int));
+    
+    //mostramos el contenido por pantalla
+    printf(" vetor = [");
+    for (int i = 0; i < MAX_TAM; i++){
+        
+        printf("%d, ", vector[i]);
+    }
+    if (MAX_TAM > 0) {
+        
+        printf("%d", vector[MAX_TAM - 1]);
+    }
+    printf("]");
+    
+    //liberamos memoria
+    free();
+    
+    
+    vector = (int *) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* HASTA AQUI LA ENTREGA OBLIGATORIA */
+    
+    return 0;
+}
